c4e53230697aca43e07de34fcef0ec59e09b4442
Letícia dos Santos <santos.leti@hotmail.com>
2020-10-24 16:57:32 -0300

    imports padrão

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+import math
+import pandas as pd
+import random
+import numpy as np
+#from arvore import Arvore, FlorestaAleatoria
+from multiprocessing import Pool
+import time
+
+class neuronio:
+    def __init__(self):
+        self.pesos={}
+
+def main():
+    print("na main\n")
+    return
+
+if __name__ == "__main__":
+    main()
